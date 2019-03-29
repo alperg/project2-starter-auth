a7eafcc73f563bca6beea3fd3921d60d4a8b3321
Alper Gokcehan <mail@alperg.com>
2019-03-28 20:56:06 -0400

    linting configuration fixes

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(helmet.hsts({
 // catch 404 and forward to error handler
 if (app.get('env') !== 'development') {
   app.use((req, res, next) => {
-    let err = new Error('Not Found: ' + req.url);
+    const err = new Error('Not Found: ' + req.url);
     err.status = 404;
     next(err);
   });
