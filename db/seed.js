e79702fe20eadf77d6a26a53fca3d02f41cf498d
Alper Gokcehan <mail@alperg.com>
2020-09-26 09:08:40 -0400

    some updates

--- db/seed.js
+++ db/seed.js
@@ -1,16 +1,23 @@
 module.exports = (db) => {
   db.User.create({
-    firstName: 'Joe',
+    firstName: 'Adam',
     lastName: 'Gates',
-    email: 'j@g.co',
+    email: 'adam@gates.com',
     password: process.env.ADMIN_USER_PWD,
     isAdmin: true
-  });
-  db.User.create({
-    firstName: 'Jane',
-    lastName: 'Jobs',
-    email: 'j@j.co',
-    password: process.env.USER_PWD,
-    isAdmin: false
+  }).then(() => {
+    db.User.create({
+      firstName: 'Uma',
+      lastName: 'Pearson',
+      email: 'uma@pearson.com',
+      password: process.env.USER_PWD,
+      isAdmin: false
+    }).then(() => {
+      db.Example.create({
+        text: 'Sample item',
+        description: 'Adam can\'t see this',
+        UserId: 2
+      });
+    });
   });
 };
